d9b0957ac404f2a68536ea3126ffa30f9dcfada2
Eugene <eugeneayonga@gmail.com>
2022-06-11 15:31:09 +0300

    commit tree 04abecaf57a33a49ce81053529b18ee16173ef20

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(customer) {
+    window.bestCustomer = customer;
+}
+
+
+const leastFavoriteCustomer = 'not bob';
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+}
